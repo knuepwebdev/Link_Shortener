942a541e6666adcde01ca1975a0613847fb3387b
hallmarklabs <hallmarklabs@tech-mknueppel.attlocal.net>
2018-08-14 19:13:42 -0700

    Prepend short URLs

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,12 +7,12 @@ class Link < ActiveRecord::Base
   # create a unique short URL
   def generate_short_url
     random_chars = ['0'..'9','A'..'Z','a'..'z'].map{|range| range.to_a}.flatten
-    self.assign_attributes(short_url: 6.times.map{ random_chars.sample }.join) until self.short_url.present? && Link.find_by_short_url(short_url).nil?
+    self.assign_attributes(short_url: 6.times.map{ random_chars.sample }.join.prepend("http://")) until self.short_url.present? && Link.find_by_short_url(short_url).nil?
   end
 
   def generate_admin_url
     random_chars = ['0'..'9','A'..'Z','a'..'z'].map{|range| range.to_a}.flatten
-    self.assign_attributes(admin_url: 6.times.map{ random_chars.sample }.join) until self.admin_url.present? && Link.find_by_admin_url(short_url).nil?
+    self.assign_attributes(admin_url: 6.times.map{ random_chars.sample }.join.prepend("http://")) until self.admin_url.present? && Link.find_by_admin_url(short_url).nil?
   end
 
   def validate_long_url
